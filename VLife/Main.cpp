fa83c55c75cf8a7da3a0670e7a36eb3c8f3ac55c
Robert Arifulin <robertarif2015@gmail.com>
2022-08-11 01:22:08 +0300

    little texture fun)

--- VLife/Main.cpp
+++ VLife/Main.cpp
@@ -1,4 +1,5 @@
-#include "SDL.h"
+#include <SDL.h>
+#include <SDL_Image.h>
 #include <cstdio>
 #include <iostream>
 
@@ -6,106 +7,121 @@ const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 SDL_Window* win = NULL;
-SDL_Surface* scr = NULL;
-SDL_Surface* smile = NULL;
+SDL_Renderer* ren = NULL;
+SDL_Texture* flower = NULL;
 
 
-int init() {
+bool init() {
+    bool ok = 1;
+
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        std::cout << "Can't init: " << SDL_GetError() << std::endl;
-        system("pause");
-        return 1;
+        std::cout << "can't init: " << SDL_GetError() << '\n';
+        ok = false;
     }
 
-    win = SDL_CreateWindow("События", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    win = SDL_CreateWindow("Texture", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (win == NULL) {
-        std::cout << "Can't create window: " << SDL_GetError() << std::endl;
-        system("pause");
-        return 1;
+        ok = 0;
+        std::cout << "can't create window " << SDL_GetError() << '\n';
     }
 
-    scr = SDL_GetWindowSurface(win);
+    ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (ren == NULL) {
+        std::cout << "can't create render " << SDL_GetError() << '\n';
+        ok = 0;
+    }
 
-    return 0;
+    SDL_SetRenderDrawColor(ren, 0xFF, 0xFF, 0xFF, 0xFF);
+
+    int flags = IMG_INIT_PNG;
+    if (!(IMG_Init(flags) & flags)) {
+        std::cout << "can't init IMG " << SDL_GetError() << '\n';
+        ok = 0;
+    }
+
+    return ok;
 }
 
 
-int load() {
-    smile = SDL_LoadBMP("smile.bmp");
-
-    if (smile == NULL) {
-        std::cout << "Can't load image: " << SDL_GetError() << std::endl;
-        system("pause");
-        return 1;
+bool load() {
+    bool ok = 1;
+    SDL_Surface*  temp_surf = NULL;
+    temp_surf = IMG_Load("images/flower.png");
+    if (temp_surf == NULL) {
+        std::cout << "Can't load FLOWER IMG : " << IMG_GetError() << std::endl;
+        ok = 0;
     }
 
-    return 0;
+
+    flower = SDL_CreateTextureFromSurface(ren, temp_surf);
+    if (flower == NULL) {
+        std::cout << "Can't create texture : " << SDL_GetError() << std::endl;
+        ok = 0;
+    }
+
+    return ok;
 }
+
 
 int quit() {
-    SDL_FreeSurface(smile);
-
     SDL_DestroyWindow(win);
+    win = NULL;
+
+    SDL_DestroyRenderer(ren);
+    ren = NULL;
+
+    SDL_DestroyTexture(flower);
+    flower = NULL;
 
     SDL_Quit();
+    IMG_Quit();
 
     return 0;
 }
 
-
 int main(int argc, char** args) {
-    if (init() == 1) {
-        return 1;
 
-    }
-
-    if (load() == 1) {
+    if (!init()) {
+        quit();
         return 1;
     }
 
-    bool run = true;
+    if (!load()) {
+        quit();
+        return 1;
+    }
+
+    bool run = 1;
     SDL_Event e;
-    SDL_Rect r;
 
-    int x = 300;
-    int y = 300;
+    SDL_Rect Tr;
+    SDL_Rect TCord;
 
-    r.x = x;
-    r.y = y;
+    Tr.w = 50;
+    Tr.h = 50;
+    Tr.x = 0;
+    Tr.y = 0;
+    TCord.w = 50;
+    TCord.h = 50;
 
+    SDL_RenderClear(ren);
     while (run) {
-        while (SDL_PollEvent(&e) != NULL) {
-            if (e.type == SDL_QUIT) {
-                run = false;
-            }
-            if (e.type == SDL_KEYDOWN) {
-                switch (e.key.keysym.sym) {
-
-                case SDLK_UP:
-                    y -= 40;
-                    break;
-                case SDLK_DOWN:
-                    y += 40;
-                    break;
-                case SDLK_RIGHT:
-                    x += 40;
-                    break;
-                case SDLK_LEFT:
-                    x -= 40;
-                    break;
-                }
-            }
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT)
+                run = 0;
+            if (e.type == SDL_MOUSEBUTTONDOWN) {
+                std::cout << "MSB D \n" << Tr.x << " " << Tr.y << '\n';
+                Uint32 buttons = SDL_GetMouseState(&Tr.x, &Tr.y);
+                Tr.x -= 25;
+                Tr.y -= 25;
+             }
         }
-
-        r.x = x;
-        r.y = y;
-
-        SDL_FillRect(scr, NULL, SDL_MapRGB(scr->format, 255, 255, 255));
-
-        SDL_BlitSurface(smile, NULL, scr, &r);
-
-        SDL_UpdateWindowSurface(win);
+        
+        SDL_RenderCopy(ren, flower, NULL, &Tr);
+        SDL_RenderPresent(ren);
     }
 
-    return quit();
-}
+    quit();
+
+    return 0;
+};
